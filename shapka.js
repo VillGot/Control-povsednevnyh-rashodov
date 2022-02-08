572ff56ff406c89ee6ea0d4705ce08cf99cbe79f
Илья Демидов <mimickthan@yandex.ru>
2022-02-09 00:07:45 +0300

    Сделаны основные странички. Вход, регистрация, главная, основная и временный бекенд

---
+++ shapka.js
@@ -0,0 +1,35 @@
+document.write("<header class='d-flex justify-content-between'>\
+<div class='d-inline-flex'>\
+\
+    <svg id='logo' data-name='Слой 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 175.87 175.87'>\
+        <defs>\
+            <style>\
+                .cls-1,\
+                .cls-2 {\
+                    fill: none;\
+                    stroke-miterlimit: 10;\
+                }\
+\
+                .cls-1 {\
+                    stroke: #5da5e9;\
+                    stroke-width: 15px;\
+                }\
+\
+                .cls-2 {\
+                    stroke: #0078e9;\
+                    stroke-width: 5px;\
+                }\
+            </style>\
+        </defs>\
+        <title>Лого 2</title>\
+        <circle class='cls-1' cx='87.93' cy='87.93' r='80.43' />\
+        <line class='cls-1' x1='87.93' y1='7.5' x2='87.93' y2='168.37' />\
+        <line class='cls-2' x1='137.4' y1='151.36' x2='87.93' y2='7.5' />\
+        <line class='cls-2' x1='87.93' y1='7.5' x2='38.41' y2='151.32' />\
+        <line class='cls-2' x1='118.48' y1='95.69' x2='57.39' y2='95.69' />\
+    </svg>\
+\
+    <div class='vertical-line1'></div>\
+    <div class='header-information'>Финансовый<br>ассистент</div>\
+</div>\
+</header>");
